53cdec19164fc64ca96e0af1d840d7be9ecc4cc8
zz <zoloto@elecon.kiev.ua>
2012-12-19 18:39:57 +0200

    Add admin url and work but only auth

--- firstsite/urls.py
+++ firstsite/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from firstsite.views import hello
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
+from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -11,8 +11,8 @@ urlpatterns = patterns('',
     # url(r'^firstsite/', include('firstsite.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
