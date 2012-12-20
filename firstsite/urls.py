3900e69e01d98edc0088d778970fe4ab6eafbedd
zz <b@bcomp.(none)>
2012-12-21 01:22:11 +0200

    work without blog

--- firstsite/urls.py
+++ firstsite/urls.py
@@ -16,6 +16,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    /*url(r'^blog/', include('firstsite.urls')),*/
+#    url(r'^blog/', include('firstsite.urls')),
 
 )
